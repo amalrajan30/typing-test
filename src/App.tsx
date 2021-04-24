8e3cad23364fe24b123ae84712d809fe121fd753
Amal <amalrajan30@gmail.com>
2021-04-24 11:49:03 +0530

    First working state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,75 @@
-import * as React from 'react';
-import './App.css';
-import Typing from './components/Typing';
+import * as React from 'react'
+import './App.css'
+import Typing from './components/Typing'
+import {TypingArea} from './components/TypingArea'
 import CommonWords from './data/commonWords.json'
 
+export interface Words {
+  letters: Chars[]
+}
+export interface Chars {
+  char: string
+  index: number
+  wordIndex: number
+  typed: boolean
+  valid: boolean
+}
 function App() {
-	const [currentWords, setCurrentWords] = React.useState(20)
-	const words = CommonWords.commonWords.sort(() => 0.5 - Math.random()).slice(0, 200).map(word => `${word} `)
+  const [currentWords, setCurrentWords] = React.useState(20)
+  const createChars = (para: string): Chars[] => {
+    return [...para].reduce(function createCharsReducer(
+      acc: Chars[],
+      current,
+      index,
+    ) {
+      const temp: Chars = {
+        wordIndex: 0,
+        char: current,
+        index: index,
+        typed: false,
+        valid: false,
+      }
+      temp.wordIndex =
+        index === 0
+          ? 0
+          : acc[index - 1].char === ' '
+          ? acc[index - 1].wordIndex + 1
+          : acc[index - 1].wordIndex
+      return [...acc, temp]
+    },
+    [])
+  }
 
-	return (
-		<div className="App">
-			<Typing typingPara={words.slice(0, currentWords)} words={true} />
-		</div>
-	);
+  const createTypings = (words: string[]): Words[] => {
+    let index = 0
+    return words.map((word, wordIndex) => {
+      const currentWord = wordIndex === words.length - 1 ? word.trim() : word
+      return {
+        letters: [...currentWord].map(letter => {
+          return {
+            wordIndex,
+            char: letter,
+            index: index++,
+            typed: false,
+            valid: false,
+          }
+        }),
+      }
+    })
+  }
+  const words = CommonWords.commonWords
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 20)
+    .map(word => `${word} `)
+  // const chars = createChars(words)
+  const typings = createTypings(words)
+
+  return (
+    <div className="App">
+      {/* <Typing typingPara={words.slice(0, currentWords)} words={true} /> */}
+      <TypingArea typings={typings} />
+    </div>
+  )
 }
 
-export default App;
+export default App
