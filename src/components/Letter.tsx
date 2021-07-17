299d47344de88d7d56c0b3134feb68d5339da7d5
Amal <amalrajan30@gmail.com>
2021-07-17 17:51:51 +0530

    Working letter component

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react'
 import classNames from 'classnames'
-import {Chars} from '../App'
+import {Char} from '../App'
 
 type Props = {
-  letter: Chars
+  letter: Char
   position: number
   wrongLetter: undefined | string
 }
-export default function Letter(props: Props) {
-  const {letter} = props
+function LetterComponent(props: Props) {
+  const {letter, position, wrongLetter} = props
   const {char, index, typed, valid} = letter
+  const [character, setCharacter] = React.useState(char)
+  React.useEffect(() => {
+    if (position - 1 === index && !valid && typed && wrongLetter) {
+      setCharacter(wrongLetter)
+    } else setCharacter(char)
+  }, [char, index, position, typed, valid, wrongLetter])
   const letterClassName = classNames('letter', {
     current: props.position === index,
     correct: typed && valid,
@@ -17,9 +23,16 @@ export default function Letter(props: Props) {
   })
   return (
     <>
-      <span className={letterClassName}>
-        {(props.wrongLetter && index === props.position) || char}
-      </span>
+      <span className={letterClassName}>{character}</span>
     </>
   )
 }
+
+// const Letter = React.memo(
+//   LetterComponent,
+//   (perv: Props, next: Props) =>
+//     next.position !== perv.letter.index &&
+//     next.letter.valid !== perv.letter.valid,
+// )
+
+export default LetterComponent
