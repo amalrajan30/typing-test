7ef6d8bb4d2437f4b77dcdbedaa744a6ed764e97
Amal <amal@techionsoft.com>
2022-01-16 18:45:43 +0530

    Show result after test is complete

---
+++ src/components/Result/index.tsx
@@ -0,0 +1,30 @@
+import './style.css'
+
+interface IResultProps {
+  speed: string
+  accuracy: string
+  reset: () => void
+}
+
+function Result(props: IResultProps) {
+  const {speed, accuracy, reset} = props
+  return (
+    <main className="result">
+      <section>
+        <span>
+          <h3>Speed:</h3>
+          <span>{speed}</span>
+        </span>
+        <span>
+          <h3>Accuracy:</h3>
+          <span>{accuracy}</span>
+        </span>
+      </section>
+      <button className="button" onClick={reset}>
+        Restart
+      </button>
+    </main>
+  )
+}
+
+export default Result
