cbf4cbecc022c09e7a8925f3e60da5015f69fea4
Amal <amalrajan30@gmail.com>
2022-01-16 18:45:10 +0530

    Add footer, navbar

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './style.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made with{' '}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{' '}
+        by{' '}
+        <a
+          href="https://www.linkedin.com/in/amal-rajan-7b630913a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Amal
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function NavBar() {
+  return (
+    <header className="header">
+      <div>
+        <h1>Typing Test</h1>
+        <button className="button">Config</button>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
